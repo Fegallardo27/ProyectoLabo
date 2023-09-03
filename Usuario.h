8ac0b822e145781a19ce0c49718dbaa56f333bb5
Fegallardo27 <143563846+Fegallardo27@users.noreply.github.com>
2023-09-03 19:26:32 -0300

    Add files via upload

---
+++ Usuario.h
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+return 0;
+
+}#ifndef USUARIO_H
+#define USUARIO_H
+
+
+class Usuario
+{
+	public:
+		Usuario();
+
+	protected:
+
+	private:
+};
+
+#endif // USUARIO_H
